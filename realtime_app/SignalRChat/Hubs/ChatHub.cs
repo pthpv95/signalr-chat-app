ef820fd9f10c65c6575d052aae177a563756b8b2
Hien Pham <hien.pham@unicorn-fintech.com>
2019-10-27 16:29:19 +0700

    Init signalr server to broadcast messages

---
+++ realtime_app/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace realtime_app.SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("broadcastMessage", message);
+        }
+    }
+}
